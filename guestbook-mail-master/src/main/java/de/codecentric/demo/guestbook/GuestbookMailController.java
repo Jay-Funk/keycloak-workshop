c6c1fd28b237eed093a654231e3dc4542a244ff6
John Funk <john.funk@pwc.com>
2020-02-26 10:54:00 +0100

    add install.sh, updated GuestbookMailController not to send an email

--- guestbook-mail-master/src/main/java/de/codecentric/demo/guestbook/GuestbookMailController.java
+++ guestbook-mail-master/src/main/java/de/codecentric/demo/guestbook/GuestbookMailController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.PUT})
 public class GuestbookMailController {
 
-	private final MailSender sender;
+	// private final MailSender sender;
 	
 	@Value("${mail.from}")
 	private String from;
@@ -26,29 +26,32 @@ public class GuestbookMailController {
 	@Value("${mail.to}")
 	private String to;
 
-	@Autowired
-	public GuestbookMailController(MailSender sender) {
-		this.sender = sender;
-	}
+	// @Autowired
+	// public GuestbookMailController(MailSender sender) {
+	// 	this.sender = sender;
+	// }
 
 	@PostMapping(value = "/mail",
 			consumes=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Boolean> sendMail(@RequestBody GuestbookEntry entry, Principal p) {
-		Boolean isSent = false;
+		Boolean isSent = true;
+
+		System.out.println("Send email from " + from + " to " + to + ".");
+		System.out.println(entry.getCommenter() + " schrieb:\r\n\r\n" + entry.getComment() + "\r\n");
 		//Map<String, Object> customAttributes = ((KeycloakAuthenticationToken) p).getAccount().getKeycloakSecurityContext().getToken().getOtherClaims();
-		SimpleMailMessage msg = new SimpleMailMessage();
-		msg.setFrom(from);
-		msg.setTo(to);
-		msg.setSubject("[Guestbook Entry] " + entry.getTitle());
-		msg.setText(entry.getCommenter() + " schrieb:\r\n\r\n" + entry.getComment() + "\r\n");
+		// SimpleMailMessage msg = new SimpleMailMessage();
+		// msg.setFrom(from);
+		// msg.setTo(to);
+		// msg.setSubject("[Guestbook Entry] " + entry.getTitle());
+		// msg.setText(entry.getCommenter() + " schrieb:\r\n\r\n" + entry.getComment() + "\r\n");
 		
-		sender.send(msg);
-		try {
-			Thread.sleep(1500);
-			isSent = true;
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		// sender.send(msg);
+		// try {
+		// 	Thread.sleep(1500);
+		// 	isSent = true;
+		// } catch (InterruptedException e) {
+		// 	e.printStackTrace();
+		// }
 		
 		return ResponseEntity.ok().body(isSent);
 	}
